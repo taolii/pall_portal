e143b114ca98047baf1f58b865322e8376639d20
mxz <mengxianzhu@xincheng.com>
2018-05-13 18:51:14 +0800

    化学镀膜代码提交

--- pall_portal_controler/src/main/resources/static/dist/workflow/platedfilm/js/platedFilmManage.js
+++ pall_portal_controler/src/main/resources/static/dist/workflow/platedfilm/js/platedFilmManage.js
@@ -86,6 +86,10 @@ $(document).ready(function() {
         format: 'YYYY-MM-DD',  
         locale: moment.locale('zh-cn')  
     }); 
+	$('#queryPfTime').datetimepicker({  
+        format: 'YYYY-MM-DD',
+        locale: moment.locale('zh-cn')  
+    }); 
 	$('#queryStartCoatingTime').val(currentDate(30));
 	$('#queryEndCoatingTime').val(currentDate(0));
 	var columns_setting=[
