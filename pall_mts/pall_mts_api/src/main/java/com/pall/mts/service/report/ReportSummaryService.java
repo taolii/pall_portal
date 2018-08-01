56d823eb852478a0c36efee67989cf270c75e5c4
taolii <13816763233@163.com>
2018-08-01 21:38:39 +0800

    代码更新

---
+++ pall_mts/pall_mts_api/src/main/java/com/pall/mts/service/report/ReportSummaryService.java
@@ -0,0 +1,26 @@
+package com.pall.mts.service.report;
+
+import org.springframework.stereotype.Service;
+
+import com.pall.mts.common.response.BaseTablesResponse;
+import com.pall.mts.repository.entity.report.AssemblyScrapSummaryQueryFormEntity;
+import com.pall.mts.repository.entity.report.ProductTrackingQueryFormEntity;
+
+/*
+ * 组装报废汇总服务层
+ */
+@Service
+public interface ReportSummaryService {
+	/*
+	 * 根据条件查询组装报废汇总信息
+	 * @param assemblyScrapSummaryQueryFormEntity 组装信息查询对象
+	 * @return 查询结果
+	 */
+	public BaseTablesResponse queryAssemblyScrapSummary(AssemblyScrapSummaryQueryFormEntity  assemblyScrapSummaryQueryFormEntity) throws Exception;
+	/*
+	 * 根据条件查询产品跟踪信息
+	 * @param productTrackingQueryFormEntity 产品跟踪查询对象
+	 * @return 查询结果
+	 */
+	public BaseTablesResponse queryProductTracking(ProductTrackingQueryFormEntity productTrackingQueryFormEntity) throws Exception;
+}
